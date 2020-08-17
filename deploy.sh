83ad154a03b5c3e036ed9a08d5d0af879d78bd9b
SasukeBo <809754210@qq.com>
2020-08-17 20:05:19 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,21 @@ compose_path="/home/sasukebo/pmes-compose"
 
 case $1 in
 "device")
-  p back
+  cd $GOPATH/src/github.com/SasukeBo/pmes-data-center
   git pull origin version2
   cd $compose_path
   dc restart device
   ;;
 "front")
   p front
+  cd /home/sasukebo/pmes-data-center-vue
   git pull origin version2
   yarn build
   cd $compose_path
   dc restart data_center_vue
   ;;
 "back")
-  p back
+  cd $GOPATH/src/github.com/SasukeBo/pmes-data-center
   git pull origin version2
   cd $compose_path
   dc restart data_center_go
