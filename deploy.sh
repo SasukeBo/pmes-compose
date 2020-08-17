227eec3a1b2224e87be65e4f7d5885c7a6fd5092
SasukeBo <809754210@qq.com>
2020-08-17 20:06:53 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,20 @@ case $1 in
   cd $GOPATH/src/github.com/SasukeBo/pmes-data-center
   git pull origin version2
   cd $compose_path
-  dc restart device
+  docker-compose restart device
   ;;
 "front")
   cd /home/sasukebo/pmes-data-center-vue
   git pull origin version2
   yarn build
   cd $compose_path
-  dc restart data_center_vue
+  docker-compose restart data_center_vue
   ;;
 "back")
   cd $GOPATH/src/github.com/SasukeBo/pmes-data-center
   git pull origin version2
   cd $compose_path
-  dc restart data_center_go
+  docker-compose restart data_center_go
   ;;
 *)
   echo "Illegal option $1, only support device, front, back"
